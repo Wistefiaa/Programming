00a46a705c9a98a704686b49ea7a50bb86bf1764
Nipitphon Cheawchatchawan <yukanzakino@gmail.com>
2024-10-25 23:43:32 +0700

    Wistefia

---
+++ 0007Herman/0007Herman.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  double t;
+  double e;
+  int r;
+  cin>>r;
+  t = 2*r*r;
+  e = M_PI*r*r;
+  cout<<fixed<<setprecision(6)<<e<<endl<<t;
+}
---
+++ 0008X2/0008X2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int s;
+  int x1;
+  int x2;
+  cin>>x1>>s; //นำเข้า 2 ตัวแปร
+  x2 = 2*s-x1;
+  cout<<x2;
+}
